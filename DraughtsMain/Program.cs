800a34e9a538a2e3ba3d97fe2ea41dac99e1aa27
Ryan Stainsby <40168130@live.napier.ac.uk>
2017-09-30 11:41:17 +0100

    Add project files.

---
+++ DraughtsMain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DraughtsMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
